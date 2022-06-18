3bb3132a41adc0af949b024aae882361783b56bb
vdBerg93 <anonymous@a.com>
2022-06-18 10:56:55 +0200

    add welcome message to root of server

--- main.py
+++ main.py
@@ -10,5 +10,11 @@ def create_request(item: Request):
     ProcessRequest(item)
     return item
 
+@app.get("/")
+def root():
+    return {"header":"This is the root of the HTTP image processing server",
+            "option 1":"Try a test message through http://localhost:8888/docs#/default/create_request_items__post",
+            "option 2":"Run several processing tests by running test_main.py from console or through your IDE"}
+
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8888)
